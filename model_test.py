762f012957ab1bac77fbd240904b9469f80eeef5
radianceeeeeeee <97012089+radianceeeeeeee@users.noreply.github.com>
2024-06-05 16:25:15 +0800

    Pushed scaled images

--- model_test.py
+++ model_test.py
@@ -1,15 +1,17 @@
-import tensorflow as tf
+import tf_keras as tfk
 import tensorflow_hub as hub
-    
-model_mobilenet = tf.keras.Sequential([
-    hub.KerasLayer("https://www.kaggle.com/models/google/mobilenet-v2/TensorFlow2/035-128-classification/2",
-                  trainable=False, input_shape=(96,96,3)),
-    tf.keras.layers.Dense(3, activation='sigmoid')
+import tensorflow as tf
+
+num_classes = 14
+
+
+
+print(tf.__version__)
+
+m = tfk.Sequential([
+    hub.KerasLayer("https://www.kaggle.com/models/google/mobilenet-v2/TensorFlow2/035-128-feature-vector/2",
+                   trainable=False),  # Can be True, see below.
+    tf.keras.layers.Dense(num_classes, activation='softmax')
 ])
 
-model_mobilenet.compile(
-    optimizer=tf.keras.optimizers.Adam(),
-              loss=tf.keras.losses.CategoricalCrossentropy(),
-              metrics=['accuracy','Recall', 'Precision', 'AUC'])
-
-print(model_mobilenet.summary())
+m.summary()
